fe2bb45ffa70584fb0aa75f74cda5c2c1f1d5f14
Michael Ray <mray5102@uni.sydney.edu.au>
2021-10-31 14:33:26 +1100

    Update GNN.py

--- Functions/GNN/GNN.py
+++ Functions/GNN/GNN.py
@@ -93,8 +93,7 @@ class Optimizer(Notification):
 
     def EpochLoop(self):
         self.Notify("EPOCHLOOP::Training")
-        
-        lossArr = []
+
         
         for epoch in range(self.Epochs):
             for n in self.DataLoader:
@@ -106,9 +105,6 @@ class Optimizer(Notification):
             self.Notify("EPOCHLOOP::Training::EPOCH " + str(epoch+1) + "/" + str(self.Epochs) + " -> Current Loss: " + str(float(self.L)))
             lossArr.append(float(self.L))
          
-        plt.hist(lossArr, bins=200)    
-        plt.savefig(/suphys/mray5102/files/TopData/user.pgadow.310845.MGPy8EG.DAOD_TOPQ1.e7058_s3126_r10724_p3980.bsm4t-21.2.164-1-0-mc16e_output_root/GNNplot.png)
-
 
     def Learning(self):
 
