c779090d330df90fd532ed9611fbc52a2009946e
woywoy123 <thomas.nommensen@coepp.org.au>
2022-01-10 01:14:48 +1100

    Ready for deployment

--- PlayGround/PathNetsOptimizer/Source/PathNetOptimizer.cpp
+++ PlayGround/PathNetsOptimizer/Source/PathNetOptimizer.cpp
@@ -1,5 +1,6 @@
 #include <torch/extension.h>
 #include <iostream>
+#include <vector>
 
 void Combinatorial(int n, int k, int num, std::vector<int>* out)
 { 
@@ -11,7 +12,7 @@ void Combinatorial(int n, int k, int num, std::vector<int>* out)
   if (k > 0){ Combinatorial(n -1, k -1, num | ( 1 << (n -1)), out); }
 }
 
-torch::Tensor PathCombination(torch::Tensor AdjMatrix, int Nodes)
+std::vector<torch::Tensor> PathCombination(torch::Tensor AdjMatrix, int Nodes)
 {
   auto options = torch::TensorOptions().device(AdjMatrix.device().type());
   torch::Tensor msk = torch::pow(2, torch::arange(Nodes, options)); 
@@ -22,6 +23,7 @@ torch::Tensor PathCombination(torch::Tensor AdjMatrix, int Nodes)
   for (int i = 1; i < Nodes; i++){Combinatorial(Nodes, i+1, 0, &Binary);}
 
   std::vector<torch::Tensor> Output; 
+  std::vector<torch::Tensor> Output_Matrix; 
   // Convert the Binary to a tensor 
   for (int i = 0; i < Binary.size(); i++)
   {
@@ -31,26 +33,17 @@ torch::Tensor PathCombination(torch::Tensor AdjMatrix, int Nodes)
     proj.index_put_({proj == combi.sum(0)}, 0); 
     proj.index_put_({proj > 0}, 1); 
     Output.push_back(proj);
+
+    torch::Tensor proj_T = proj.matmul(proj.t()); 
+    Output_Matrix.push_back(proj_T);
   }
   
   torch::Tensor out = torch::stack(Output); 
-  return out;
-}
-
-torch::Tensor FastMassMultiplication(torch::Tensor Px, torch::Tensor Py, torch::Tensor Pz, torch::Tensor E, torch::Tensor Combinations)
-{
-  torch::Tensor px = torch::square(Px.index({Combinations}).sum(0)); 
-  torch::Tensor py = torch::square(Py.index({Combinations}).sum(0)); 
-  torch::Tensor pz = torch::square(Pz.index({Combinations}).sum(0)); 
-  torch::Tensor e = torch::square(E.index({Combinations}).sum(0)); 
-  torch::Tensor m = torch::sqrt(e - px - py - pz) / 1000; 
+  torch::Tensor out2 = torch::stack(Output_Matrix); 
   
-  return m; 
+  return {out, out2};
 }
-
-
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
   m.def("PathCombination", &PathCombination, "Path Combinatorial");
-  m.def("FastMassMultiplication", &FastMassMultiplication, "Fast MassMultiplication");
 }
--- PlayGround/PathNetsOptimizer/Source/PathNetOptimizerCUDA.cpp
+++ PlayGround/PathNetsOptimizer/Source/PathNetOptimizerCUDA.cpp
@@ -3,25 +3,40 @@
 #include <vector>
 
 // CUDA forward declaration 
-std::vector<torch::Tensor> FastMassMultiplicationCUDA(torch::Tensor eta, torch::Tensor phi, torch::Tensor pt, torch::Tensor e, torch::Tensor Combinations); 
+std::vector<torch::Tensor> ToCartesianCUDA(torch::Tensor eta, torch::Tensor phi, torch::Tensor pt, torch::Tensor e); 
+torch::Tensor PathMassCartesianCUDA(torch::Tensor x, torch::Tensor y, torch::Tensor z, torch::Tensor e, torch::Tensor Combinations); 
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), "#x must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), "#x must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
-std::vector<torch::Tensor> FastMassMultiplication(torch::Tensor eta, torch::Tensor phi, torch::Tensor pt, torch::Tensor e, torch::Tensor, torch::Tensor Combinations)
+std::vector<torch::Tensor> ToCartesian(torch::Tensor eta, torch::Tensor phi, torch::Tensor pt, torch::Tensor e)
 {
 
   CHECK_INPUT(eta); 
   CHECK_INPUT(phi);
   CHECK_INPUT(pt); 
   CHECK_INPUT(e); 
-  CHECK_INPUT(Combinations);
 
-  return FastMassMultiplicationCUDA(eta, phi, pt, e, Combinations);
+  return ToCartesianCUDA(eta, phi, pt, e);
+}
+
+torch::Tensor PathMassCartesian(torch::Tensor x, torch::Tensor y, torch::Tensor z, torch::Tensor e, torch::Tensor Combinations)
+{
+
+  CHECK_INPUT(x); 
+  CHECK_INPUT(y);
+  CHECK_INPUT(z); 
+  CHECK_INPUT(e); 
+  CHECK_INPUT(Combinations); 
+
+  return PathMassCartesianCUDA(x, y, z, e, Combinations);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m){
-  m.def("FastMassMultiplication", &FastMassMultiplication, "Fast MassMultiplication");
-  m.def("FastMassMultiplicationCUDA", &FastMassMultiplicationCUDA, "Fast MassMultiplication (CUDA)");
+  m.def("ToCartesian", &ToCartesian, "Convert to Cartesian");
+  m.def("ToCartesianCUDA", &ToCartesianCUDA, "Convert Detector to Cartesian (CUDA)");
+  m.def("PathMassCartesian", &PathMassCartesian, "Convert Path to Mass"); 
+  m.def("PathMassCartesianCUDA", &PathMassCartesianCUDA, "Convert Path to Mass (CUDA)"); 
+
 }
