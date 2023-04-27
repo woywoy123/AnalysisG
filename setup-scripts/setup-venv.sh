2697100f32fa67de422584ef5e661b3e40c0b58c
Thomas Nommensen <tnom6927@wu.physics.usyd.edu.au>
2023-04-28 04:04:38 +1000

    added psutil...

--- setup-scripts/setup-venv.sh
+++ setup-scripts/setup-venv.sh
@@ -7,16 +7,15 @@ lsetup "python 3.9.14-x86_64-centos7"
 python3 -m venv PythonGNN
 source ./PythonGNN/bin/activate
 
-export CUDA_PATH=/usr/local/cuda-11.8
-export VERSION=cu118
+#export CUDA_PATH=/usr/local/cuda-11.8
+export VERSION=cpu
 export TORCH=1.13.0
 export MAX_JOBS=12
-export CC=gcc-11
-export CXX=g++-11
+#export CC=gcc-620
+#export CXX=g++-620
 
 cd ../
 bash setup.sh
 
-
-
-echo "export PythonGNN=$PWD/PythonGNN/bin/activate" >> ~/.bashrc
+echo "alias PythonGNN=$PWD/PythonGNN/bin/activate" >> ~/.bashrc
+echo "alias GNN='source $PWD/PythonGNN/bin/activate'" >> ~/.bashrc
--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 
-export CUDA_PATH=/usr/local/cuda-11.8
-export VERSION=cu118
-export TORCH=1.13.0
-export MAX_JOBS=12
-export CC=gcc-11
-export CXX=gcc-11
+#export CUDA_PATH=/usr/local/cuda-11.8
+#export VERSION=cpu
+#export TORCH=1.13.0
+#export MAX_JOBS=12
+#export CC=gcc-6.20
+#export CXX=gcc-6.20
 
 pip install torch==${TORCH} --extra-index-url https://download.pytorch.org/whl/${VERSION}
 pip install torch_scatter -f https://data.pyg.org/whl/torch-${TORCH}+${VERSION}.html
