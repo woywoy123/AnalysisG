156754a4d8dee89ddfdb19dfebe535a061765912
woywoy123 <thomas.nommensen@coepp.org.au>
2023-06-17 18:18:33 +1000

    Full version testing

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM nvidia/cuda:11.8.0-runtime-ubuntu22.04
+
+ENV LANG=C.UTF-8
+
--- setup.py
+++ setup.py
@@ -3,37 +3,35 @@ from Cython.Build import cythonize
 
 ext_mod = [
     Extension(
-                name = "AnalysisG.Templates.ParticleTemplate", 
+                name = "AnalysisG.Templates.ParticleTemplate",
                 sources = [
-                    "src/Templates/Cython/Particle.pyx", 
+                    "src/Templates/Cython/Particle.pyx",
                     "src/Templates/CXX/Templates.cxx",
-                    "src/Templates/CXX/Tools.cxx", 
-                ], 
-    ), 
+                    "src/Templates/CXX/Tools.cxx",
+                ],
+    ),
     Extension(
                 name = "AnalysisG.Templates.EventTemplate",
                 sources = [
                     "src/Templates/Cython/Event.pyx",
                     "src/Templates/CXX/Templates.cxx",
-
                     "src/Templates/CXX/Tools.cxx",
                 ],
     ),
     Extension(
-                name = "AnalysisG.Tracer", 
+                name = "AnalysisG.Tracer",
                 sources = [
-                    "src/Templates/Cython/ROOT.pyx", 
+                    "src/Templates/Cython/ROOT.pyx",
                     "src/Templates/CXX/ROOT.cxx",
-
-                    "src/Templates/CXX/Tools.cxx", 
-                ], 
+                    "src/Templates/CXX/Tools.cxx",
+                ],
     ),
     Extension(
-                name = "AnalysisG._Tools", 
+                name = "AnalysisG._Tools",
                 sources = [
-                    "src/Templates/Cython/Tools.pyx", 
-                    "src/Templates/CXX/Tools.cxx", 
-                ], 
+                    "src/Templates/Cython/Tools.pyx",
+                    "src/Templates/CXX/Tools.cxx",
+                ],
     ),
 ]
 
