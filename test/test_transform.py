56a66532bf5c56484a8b8c1c51cec6e110b22060
Thomas Nommensen <tnom6927@wu.physics.usyd.edu.au>
2023-08-01 23:33:42 +1000

    forgot to add test transform.

--- test/test_transform.py
+++ test/test_transform.py
@@ -1,9 +1,11 @@
 from neutrino_reconstruction.common import *
+import torch
 import pyc
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def test_transform_px():
     d1 = create_tensor_cpu_1d()
-    d1_cu = d1.to(device = "cuda")
+    d1_cu = d1.to(device = device)
     p1 = create_vector_polar(1, 2, 3, 4)
     assert rounder(pyc.Transform.Px(d1[:, 0], d1[:, 2]), p1.px)
     assert rounder(pyc.Transform.Px(d1), p1.px)
@@ -14,7 +16,7 @@ def test_transform_px():
 
 def test_transform_py():
     d1 = create_tensor_cpu_1d()
-    d1_cu = d1.to(device = "cuda")
+    d1_cu = d1.to(device = device)
     p1 = create_vector_polar(1, 2, 3, 4)
     assert rounder(pyc.Transform.Py(d1[:, 0], d1[:, 2]), p1.py)
     assert rounder(pyc.Transform.Py(d1), p1.py)
@@ -25,7 +27,7 @@ def test_transform_py():
 
 def test_transform_pz():
     d1 = create_tensor_cpu_1d()
-    d1_cu = d1.to(device = "cuda")
+    d1_cu = d1.to(device = device)
     p1 = create_vector_polar(1, 2, 3, 4)
     assert rounder(pyc.Transform.Pz(d1[:, 0], d1[:, 1]), p1.pz)
     assert rounder(pyc.Transform.Pz(d1), p1.pz)
@@ -36,7 +38,7 @@ def test_transform_pz():
 
 def test_transform_pxpypz():
     d1 = create_tensor_cpu_1d()
-    d1_cu = d1.to(device = "cuda")
+    d1_cu = d1.to(device = device)
     p1 = create_vector_polar(1, 2, 3, 4)
     p1 = [p1.px, p1.py, p1.pz]
     assert rounder_l(pyc.Transform.PxPyPz(d1[:, 0], d1[:, 1], d1[:, 2]), p1)
@@ -48,7 +50,7 @@ def test_transform_pxpypz():
 
 def test_transform_pxpypze():
     d1 = create_tensor_cpu_1d()
-    d1_cu = d1.to(device = "cuda")
+    d1_cu = d1.to(device = device)
     p1 = create_vector_polar(1, 2, 3, 4)
     p1 = [p1.px, p1.py, p1.pz, p1.e]
     assert rounder_l(pyc.Transform.PxPyPzE(d1[:, 0], d1[:, 1], d1[:, 2], d1[:, 3]), p1)
@@ -60,7 +62,7 @@ def test_transform_pxpypze():
 
 def test_transform_pt():
     d1 = create_tensor_cpu_1d()
-    d1_cu = d1.to(device = "cuda")
+    d1_cu = d1.to(device = device)
     p1 = create_vector_cartesian(1, 2, 3, 4).pt
     assert rounder(pyc.Transform.Pt(d1[:, 0], d1[:, 1]), p1)
     assert rounder(pyc.Transform.Pt(d1), p1)
@@ -71,7 +73,7 @@ def test_transform_pt():
 
 def test_transform_eta():
     d1 = create_tensor_cpu_1d()
-    d1_cu = d1.to(device = "cuda")
+    d1_cu = d1.to(device = device)
     p1 = create_vector_cartesian(1, 2, 3, 4).eta
     assert rounder(pyc.Transform.Eta(d1[:, 0], d1[:, 1], d1[:, 2]), p1)
     assert rounder(pyc.Transform.Eta(d1), p1)
@@ -82,7 +84,7 @@ def test_transform_eta():
 
 def test_transform_phi():
     d1 = create_tensor_cpu_1d()
-    d1_cu = d1.to(device = "cuda")
+    d1_cu = d1.to(device = device)
     p1 = create_vector_cartesian(1, 2, 3, 4).phi
     assert rounder(pyc.Transform.Phi(d1[:, 0], d1[:, 1]), p1)
     assert rounder(pyc.Transform.Phi(d1), p1)
@@ -93,7 +95,7 @@ def test_transform_phi():
 
 def test_transform_ptetaphi():
     d1 = create_tensor_cpu_1d()
-    d1_cu = d1.to(device = "cuda")
+    d1_cu = d1.to(device = device)
     p1 = create_vector_cartesian(1, 2, 3, 4)
     p1 = [p1.pt, p1.eta, p1.phi]
     assert rounder_l(pyc.Transform.PtEtaPhi(d1[:, 0], d1[:, 1], d1[:, 2]), p1)
@@ -105,7 +107,7 @@ def test_transform_ptetaphi():
 
 def test_transform_ptetaphie():
     d1 = create_tensor_cpu_1d()
-    d1_cu = d1.to(device = "cuda")
+    d1_cu = d1.to(device = device)
     p1 = create_vector_cartesian(1, 2, 3, 4)
     p1 = [p1.pt, p1.eta, p1.phi, p1.e]
     assert rounder_l(pyc.Transform.PtEtaPhiE(d1[:, 0], d1[:, 1], d1[:, 2], d1[:, 3]), p1)
