5c55a9dd1142bb586a5fa72011de907cc052a3d3
woywoy123 <33817290+woywoy123@users.noreply.github.com>
2025-11-19 15:26:28 +0000

    sync1

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+master_doc = 'index'
 
 # -- Breathe Configuration --------------------------------------------------
 breathe_projects = {
