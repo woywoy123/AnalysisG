227ce6ad47d0fed093edb32d7381793905d3fa65
woywoy123 <33817290+woywoy123@users.noreply.github.com>
2025-11-19 15:07:33 +0000

    sync

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+# -- Project information -----------------------------------------------------
+project = 'AnalysisG'
+copyright = '2024, Dein Name'
+author = 'Dein Name'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'breathe',
+]
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Breathe Configuration --------------------------------------------------
+breathe_projects = {
+    "AnalysisG": "../doxygen-docs/xml"
+}
+breathe_default_project = "AnalysisG"
+
+# -- Language ---------------------------------------------------------------
+language = 'en'
